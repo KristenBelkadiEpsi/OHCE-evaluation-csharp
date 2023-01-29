1042f1da7c8b077c6468d35d44658137a345b55d
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-01-29 14:27:33 +0100

    modification de la méthode Mirroir dans la classe OHCE pour faire passer la methode test correspondante (TestChaineAuRevoir)

--- OHCE-evaluation/OHCE.cs
+++ OHCE-evaluation/OHCE.cs
@@ -8,18 +8,18 @@ namespace OHCE_evaluation
 {
     internal class OHCE
     {
-
         public string Mirroir(string chaine)
         {
             string chaineMirroir = new string(chaine.Reverse().ToArray());
             if (chaine == chaineMirroir)
             {
-                return "Bonjour" + chaineMirroir + "Bien dit";
+                return "Bonjour " + chaineMirroir + " Bien dit Au revoir";
             }
             else
             {
-                return "Bonjour" + chaineMirroir;
+                return "Bonjour " + chaineMirroir + " Au revoir";
             }
+
         }
 
 
