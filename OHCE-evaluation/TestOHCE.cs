26851dc726f8c9fd9965335309a20837698b453a
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-01-29 14:07:35 +0100

    écriture du corps de la fonction Test TestChaineMirroir => elle échoue

--- OHCE-evaluation/TestOHCE.cs
+++ OHCE-evaluation/TestOHCE.cs
@@ -12,5 +12,17 @@ namespace OHCE_evaluation
             //ALORS celle-ci est renvoyé en mirroir
             Assert.Contains("eértne'd eniahc enu", chaineMirroir);
         }
+        [Fact(DisplayName = "QUAND on saisit un palindrome ALORS celui-ci est renvoyé ET « Bien dit » est envoyé ensuite")]
+        public void TestChainePalindrome()
+        {
+            OHCE ohce = new OHCE();
+            //QUAND on saisit un palindrome
+            string chaineMirroir = ohce.Mirroir("kayak");
+            //ALORS celui-ci est renvoyé ET « Bien dit » est envoyé ensuite
+            Assert.Contains("kayak", chaineMirroir);
+            Assert.Contains("Bien dit", chaineMirroir);
+
+
+        }
     }
 }
