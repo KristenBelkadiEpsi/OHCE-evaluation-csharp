db27627813ef82131596a050107656b179fa15bc
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-01-29 12:29:50 +0100

    énonciation du premier test: QUAND on saisit une chaine ALORS celle-ci est renvoyé en miroir

---
+++ OHCE-evaluation/TestOHCE.cs
@@ -0,0 +1,11 @@
+namespace OHCE_evaluation
+{
+    public class TestOHCE
+    {
+        [Fact(DisplayName = "QUAND on saisit une chaine ALORS celle-ci est renvoyé en miroir")]
+        public void TestChaineMiroir()
+        {
+
+        }
+    }
+}
