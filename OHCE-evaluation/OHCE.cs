bc7c56af738c77ee6390d73ce55ba7f96723fb08
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-01-31 20:35:05 +0100

    création de l'enum Periode implémentant les differentes periode de la journée ainsi que modification (via surcharge) de la methode Mirroir afin que le test TestChaineBonjourLanguePeriodeMatin passe

--- OHCE-evaluation/OHCE.cs
+++ OHCE-evaluation/OHCE.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace OHCE_evaluation
@@ -60,6 +61,104 @@ namespace OHCE_evaluation
 
 
 
+        }
+
+        public string Mirroir(string chaine, Langue langue, Periode periode)
+        {
+            string salutationPeriode;
+            switch (langue)
+            {
+                case Langue.En:
+
+                    switch (periode)
+                    {
+                        case Periode.Matin:
+                            salutationPeriode = "Good morning";
+                            break;
+                        case Periode.ApresMidi:
+                            salutationPeriode = "Good afternoon";
+                            break;
+                        case Periode.Soir:
+                            salutationPeriode = "Good evening";
+                            break;
+                        case Periode.Nuit:
+                            salutationPeriode = "Good night";
+                            break;
+                        default:
+                            salutationPeriode = "Good morning";
+                            break;
+                    }
+                    break;
+                case Langue.Fr:
+                    switch (periode)
+                    {
+                        case Periode.Matin:
+                            salutationPeriode = "Bonjour";
+                            break;
+                        case Periode.ApresMidi:
+                            salutationPeriode = "Bon après-midi";
+                            break;
+                        case Periode.Soir:
+                            salutationPeriode = "Bonsoir";
+                            break;
+                        case Periode.Nuit:
+                            salutationPeriode = "Bonne nuit";
+                            break;
+                        default:
+                            salutationPeriode = "Bonjour";
+                            break;
+                    }
+                    break;
+                default:
+                    switch (periode)
+                    {
+                        case Periode.Matin:
+                            salutationPeriode = "Bonjour";
+                            break;
+                        case Periode.ApresMidi:
+                            salutationPeriode = "Bon après-midi";
+                            break;
+                        case Periode.Soir:
+                            salutationPeriode = "Bonsoir";
+                            break;
+                        case Periode.Nuit:
+                            salutationPeriode = "Bonne nuit";
+                            break;
+                        default:
+                            salutationPeriode = "Bonjour";
+                            break;
+                    }
+                    break;
+
+            }
+            string bienDit;
+            string auRevoir;
+            string chaineMirroir = new string(chaine.Reverse().ToArray());
+            switch (langue)
+            {
+                case Langue.Fr:
+                    bienDit = "Bien dit";
+                    auRevoir = "Au revoir";
+                    break;
+                case Langue.En:
+                    bienDit = "Well said";
+
+                    auRevoir = "Goodbye";
+                    break; ;
+                default:
+                    bienDit = "Bien dit";
+                    auRevoir = "Au revoir";
+                    break;
+
+            }
+            if (chaineMirroir == chaine)
+            {
+                return salutationPeriode + " " + chaineMirroir + " " + bienDit + " " + auRevoir;
+            }
+            else
+            {
+                return salutationPeriode + " " + chaineMirroir + " " + auRevoir;
+            }
         }
     }
 }
